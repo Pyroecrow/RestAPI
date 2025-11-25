a518aba1295ee973f2256c673b3bc25e08a87e25
Pyroecrow <alauritzen84@gmail.com>
2025-11-25 12:46:36 +0100

    ferdig med student api

--- studentApi/src/app.js
+++ studentApi/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const studentRoutes = require('./v1/Routes/StudentRoutes');
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 // Sample route
-app.use('/api/v1/students', );
+app.use('/api/v1/students', studentRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
